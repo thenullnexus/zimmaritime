49fcb4f07c6ac86baa753b8a508fc5686131487c
AAMohamedFahim <aafahim003@gmail.com>
2026-02-10 13:56:51 +0530

    Fix React Router basename to match GitHub Pages base path

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/zimmaritime">
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
